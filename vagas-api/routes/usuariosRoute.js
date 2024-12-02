d31b487db079ba081774baf28ac836a162d3b7bb
phplemos <phplemos.dev@gmail.com>
2024-12-02 02:11:52 -0300

    feat: implementando contexto de login do usuario e correções na api

--- vagas-api/routes/usuariosRoute.js
+++ vagas-api/routes/usuariosRoute.js
@@ -4,13 +4,17 @@ import {
   update,
   remove,
   findAll,
+  findById,
 } from "../repositories/usuarioRepository.js";
 
 const usuarioRouter = Router();
 
 usuarioRouter.post("/", (req, res) => {
-  const { nome, email, telefone, senha } = req.body;
-  let usuario = create({ nome, email, telefone, senha });
+  const { nome, email, senha } = req.body;
+  let usuario = create({ nome, email, senha });
+  if (usuario === null) {
+    return res.status(400).json({ message: "Usuário já cadastrado" });
+  }
   return res.status(201).json(usuario);
 });
 
@@ -19,19 +23,27 @@ usuarioRouter.get("/", (req, res) => {
   return res.json(usuarios);
 });
 
+usuarioRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const usuario = findById(id);
+  if (!usuario) {
+    return res.status(404).json({ message: "Usuario não encontrado", id: id });
+  }
+  return res.json(usuario);
+});
+
 usuarioRouter.put("/:id", (req, res) => {
-  const { nome, email, telefone, senha } = req.body;
+  const { nome, email, senha } = req.body;
   const { id } = req.params;
   const usuario = update(id, {
     nome,
     email,
-    telefone,
     senha,
   });
   if (!usuario) {
     return res.status(404).json({ message: "Usuario não encontrado" });
   }
-  return res.json(usuario);
+  return res.status(200).json(usuario);
 });
 
 usuarioRouter.delete("/:id", (req, res) => {
